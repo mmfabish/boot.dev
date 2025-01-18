c146f90ed0d9f40c20b1a3178623d1f0d57d784d
Matthew Fabish <mmfabish@gmail.com>
2025-01-18 15:46:57 -0500

    Created Player class.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,7 +10,10 @@ def main():
     print("Screen height: {0}".format(SCREEN_HEIGHT))
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +21,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
